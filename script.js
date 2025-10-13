03656aef63f9c26c7628579bdce4a4730f6655fb
namelessbillion-hub <chrisjuliales@gmail.com>
2025-10-13 13:54:28 +0800

    update site

--- script.js
+++
@@ -1,6 +0,0 @@
-// Fade in the main content on load
-window.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('main, header, footer').forEach(el => {
-    el.classList.add('fade-in');
-  });
-});
