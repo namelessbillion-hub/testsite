824353e88dfe077e603f25f88242527df21bdfea
namelessbillion-hub <chrisjuliales@gmail.com>
2025-10-16 14:58:51 +0800

    update 10/16 2:58

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ function copyCA() {
   navigator.clipboard.writeText(textToCopy)
     .then(() => {
         document.getElementById('copy-message').style.visibility = 'visible'; 
+        setTimeout(function() {
+            document.getElementById('copy-message').style.visibility = 'hidden'; 
+        }, 1500);
     })
     .catch(err => {
       console.error("Failed to copy text: ", err);
