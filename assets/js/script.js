e04bc65f44d28ead84d774cd10fc1eccb116ad6a
namelessbillion-hub <chrisjuliales@gmail.com>
2025-10-16 11:36:57 +0800

    update 10/16 11:36

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function copyCA() {
   const textToCopy = document.getElementById("contract-text").innerText;
   navigator.clipboard.writeText(textToCopy)
     .then(() => {
-      alert("Text copied to clipboard!");
+        document.getElementById('copy-message').style.visibility = 'visible'; 
     })
     .catch(err => {
       console.error("Failed to copy text: ", err);
