92c2ac803647949916daa5b2c116d3547595b09e
namelessbillion-hub <chrisjuliales@gmail.com>
2025-10-17 19:51:54 +0800

    update 10/17 7:51

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,18 @@ function copyCA() {
       console.error("Failed to copy text: ", err);
     });
 }
+
+function copyCABurger() {
+  const textToCopy = document.getElementById("contract-text-full-burger").innerText;
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+        document.getElementById('copy-message-burger').style.visibility = 'visible'; 
+        setTimeout(function() {
+            document.getElementById('copy-message-burger').style.visibility = 'hidden'; 
+        }, 1500);
+    })
+    .catch(err => {
+      console.error("Failed to copy text: ", err);
+    });
+}
+
